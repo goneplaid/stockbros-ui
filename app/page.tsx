4d1bd7ee3e5743d42493f9a48a3306556af1295e
Shawn Wilson <wilson.ohyeah@gmail.com>
2023-06-20 16:30:53 -0600

    style - use plain styling, not Image component, for backgrounds. add bg images to tailwind config

--- app/page.tsx
+++ app/page.tsx
@@ -2,16 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Image
-        alt="Capitalism at work!"
-        src="/images/backgrounds/stage-1.png"
-        fill
-        sizes="100vw"
-        style={{ objectFit: "cover", objectPosition: "bottom 25%" }}
-      />
-
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-stage-1 bg-no-repeat bg-cover bg-[center_bottom_30%]">
+      <div className="z-10 w-full max-w-7xl items-center justify-between text-sm lg:flex">
         <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
           Get started by editing&nbsp;
           <code className="font-heading font-bold">app/page.tsx</code>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
+        "stage-1": "url('/images/backgrounds/stage-1.png')",
+        "stage-2": "url('/images/backgrounds/stage-2.png')",
+        "stage-3": "url('/images/backgrounds/stage-3.png')",
+        "stage-4": "url('/images/backgrounds/stage-4.png')",
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
