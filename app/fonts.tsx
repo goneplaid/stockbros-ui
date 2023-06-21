3f3cb1def67b4f33fc77b70e4f2ef7773ad0a7cc
Shawn Wilson <wilson.ohyeah@gmail.com>
2023-06-20 20:10:03 -0600

    fix - google fonts loading fix

--- app/fonts.tsx
+++ app/fonts.tsx
@@ -4,24 +4,28 @@ const heading = Luckiest_Guy({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-heading",
+  display: "swap",
 });
 
 const subheading = Oswald({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-subheading",
+  display: "swap",
 });
 
 const secondaryHeading = Quicksand({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-secondary-heading",
+  display: "swap",
 });
 
 const primary = Inter({
   weight: "400",
   subsets: ["latin"],
   variable: "--font-primary",
+  display: "swap",
 });
 
 const fonts = [heading, subheading, secondaryHeading, primary];
